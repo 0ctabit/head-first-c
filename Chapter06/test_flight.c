51a1da8874c4282ab331c78d87874025859eb26c
Andranik Avakian <epistle@lavabit.com>
2019-03-14 16:41:16 +0300

    strdup func solving

---
+++ Chapter06/test_flight.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+island* create(char *name)
+{
+	island *i = malloc(sizeof(island));
+	i->name = strdup(name);
+	i->opens = "09:00";
+	i->closes = "17:00";
+	i->next = NULL;
+	return i;
+}
