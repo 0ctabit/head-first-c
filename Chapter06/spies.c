cad623c24842adccb69bc6fd4915d3d2f0128853
Andranik Avakian <epistle@lavabit.com>
2019-07-06 17:24:23 +0300

    Add some code

--- Chapter06/spies.c
+++ Chapter06/spies.c
@@ -16,3 +16,51 @@ int yes_no(char *question)
 }
 
 node* create(char *question)
+{
+	node *n = malloc(sizeof(node));
+	n->question = strdup(question);
+	n->no=NULL;
+	n->yes = NULL;
+	return n;
+}
+
+void release{node *n}
+{
+	if (n) {
+		if (n->no);		
+			release(n->no);
+		if (n->yes)
+			release(n->yes);
+		if (n->question)
+			free(n->question);
+		free(n);
+	}
+}
+int main ()
+{
+	char question[80];
+	char suspect[20];
+	node *start_node = create("Does suspect have a mustache");
+	start_node->no = create("Loretta Barnsworth");
+	start_node->yes = create("Vinny the Spoon");
+
+	node *current;
+	do {
+		current = start_node;
+		while (1) {
+			if (yes_no(current->question))
+			{
+				if (current->yes) {
+					current = current->yes;
+				}else {
+					printf("SUSPECT IDENTIFIED\n");
+					break;
+				}
+			}else if (current->no) {
+				current = current->yes;
+			}else
+		}
+	}
+
+}
+
